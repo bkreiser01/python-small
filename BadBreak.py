fb7d42d7557387a7d720d1ec03863ef2ca7188ca
Brandon <bkreiser5@gmail.com>
2019-02-20 10:57:49 -0500

    8/8/17

---
+++ BadBreak.py
@@ -0,0 +1,14 @@
+#Put your name here.
+
+while True:
+    #Prompt user and receive input.
+    print("Please enter the letter 'X'.")
+    userLetter = input()
+
+    #Handle user's input appropriately.
+    if userLetter == 'X':
+        break
+    else:
+        print("That's not what I asked for!")
+
+print("Very good!  You clearly have superior keyboarding skills.")
